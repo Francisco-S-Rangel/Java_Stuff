c9ef99317c1477bfa779e71577706d10e8aabfd2
Francisco de Souza Rangel <franciscosouza20092000@gmail.com>
2022-02-14 20:20:53 -0300

    new

---
+++ firstclass/TypesofVariables.java
@@ -0,0 +1,32 @@
+package firstclass;
+
+class TypesofVariables {
+    public static void main(String[] args) {
+
+        byte numeroMuitoPequeno;// 8 bits
+        short inteiroPequeno;// 16 bits
+        int inteiro;// 32 bits
+        long inteiroLongo;// 64 bits
+        float real;// 32 bits
+        double realDuplo;// 64 bits
+        char letra;// 16 bits
+        String string;
+        boolean booleano;
+
+        inteiro = 10;
+        string = "Fatec Mogi das Cruzes";
+
+        numeroMuitoPequeno = 0;
+        inteiroPequeno = 0;
+        inteiro = 10;
+        inteiroLongo = 100;
+        real = 10.1f;/* o Java não gosta do tipo float porem permite 
+        a utlização do mesmo contanto que se coloque um "f" para fazer referencia de float.*/
+        realDuplo = 100.20; 
+        letra = 'a';
+        string ="The world is mine!";
+        booleano = true;
+
+
+    }
+}
